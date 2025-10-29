3dcb75b485743a90c4200987c91882e1dabe3bcc
Constanza Sofia Blanco Zufiría <conblazu@gmail.com>
2025-10-29 17:47:57 -0300

    EndPointsReservasDia_Semana

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 from routes import proof
 from routes import login
 from routes import home
+from routes import showAll 
+from FastAPI.routes import dayReservations
+from routes import weekReservations
+
 
 app = FastAPI()
 
@@ -15,3 +19,5 @@ def root():
 app.include_router(proof.router)
 app.include_router(login.router)
 app.include_router(home.router)
+app.include_router(showAll.router)
+app.include_router(dayReservations.router)
---
+++ FastAPI/routes/weekReservations.py
@@ -0,0 +1,48 @@
+from fastapi import APIRouter, Depends
+from db.connector import getConnection
+from core.security import currentUser  # 👈 Importante
+
+router = APIRouter()
+
+@router.get("/weekReservations")
+def dayReservations(user=Depends(currentUser)): 
+    try:
+        cn = getConnection()
+        cur = cn.cursor(dictionary=True)
+
+        #Obtener el correo del usuario autenticado
+        correo = user["correo"]
+
+        #Busco su ci a partir del correo
+        cur.execute("""
+            SELECT ci
+            FROM participante
+            WHERE correo = %s
+        """, (correo,))
+        participante = cur.fetchone()
+
+        if not participante:
+            return {"error": "No se encontró participante asociado a este usuario"}
+
+        ci = participante["ci"]
+        #Hago la consulta de reservas en el dia sin contar las canceladas con ci
+        cur.execute("""
+            SELECT r.fecha, rp.ci_participante
+            FROM reserva r
+            JOIN reserva_participante rp ON r.id_reserva = rp.id_reserva
+            WHERE DATEDIFF(CURRENT_DATE, DATE(r.fecha)) <= 7
+            AND rp.ci_participante = %s
+            AND r.estado!='cancelada'
+        """, (ci,)) #Parametrizamos para evitar Injections
+
+        resp = cur.fetchall()
+        return {"reservas_del_usuario_en_la_semana": resp}
+
+    except Exception as e:
+        return {"error": str(e)}
+
+    finally:
+        try:
+            cn.close()
+        except:
+            pass
