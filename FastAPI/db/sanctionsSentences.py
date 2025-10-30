450b9575f9330341b74545c723c9019751eb129b
Martin <mujica0106@gmail.com>
2025-10-30 20:43:40 -0300

    Se arregla explocion

--- FastAPI/db/sanctionsSentences.py
+++ FastAPI/db/sanctionsSentences.py
@@ -9,3 +9,16 @@ def getAllSanctions():
         return cur.fetchall()
     finally:
         cn.close()
+
+def createSanction(ci: int):
+    cn = getConnection()
+    try:
+        cur = cn.cursor()
+        cur.execute("""
+            INSERT INTO sancion_participante (ci_participante, fecha_inicio, fecha_fin, created_at)
+            VALUES (%s, CURDATE(), DATE_ADD(CURDATE(), INTERVAL 2 MONTH), NOW());
+        """, (ci,))
+        cn.commit()
+        return cur.rowcount
+    finally:
+        cn.close()
