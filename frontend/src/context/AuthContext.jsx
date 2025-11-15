d84b9a482a7f2071ad51ab4d3cce72eb476ea2ce
ManuelNey <mnet.cabrera@correo.ucu.edu.uy>
2025-11-15 17:05:12 -0300

    Arreglo de endpoint ya establecidos + arreglo del init de la bd

--- frontend/src/context/AuthContext.jsx
+++
@@ -1,8 +0,0 @@
-// src/context/AuthContext.jsx
-import { createContext, useContext } from "react";
-
-export const AuthContext = createContext(null);
-
-export function useAuth() {
-  return useContext(AuthContext);
-}
