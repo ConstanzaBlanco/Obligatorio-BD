e47c2536929105c71a24c09e4757dfb803157891
Martin <mujica0106@gmail.com>
2025-11-17 20:40:05 -0300

    Se agrega mas seguridad y se arregla un par de bugs

--- frontend/src/protect/Protected.jsx
+++ frontend/src/protect/Protected.jsx
@@ -1,12 +1,12 @@
 import { Outlet, Navigate } from "react-router-dom";
-import { useUser } from "./components/UserContext";
+import { useUser } from "../components/UserContext";
 
 export default function Protected() {
   const { user, loadingUser } = useUser();
 
   if (loadingUser) return <p>Cargando...</p>;
 
-  if (!user) return <Navigate to="/login" replace />;
+  if (!user) return <Navigate to="/" replace />;
 
   return <Outlet />;
 }
